cef64fb77998711202bde869a9535cde62550295
Matheus Morcinek <matheus.morcinek@Matheuss-MacBook-Pro.local>
2020-10-05 14:46:51 -0300

    example 3

--- index.js
+++ index.js
@@ -13,6 +13,6 @@ console.log(numbersArray);
 // const num1 = 1;
 // const num2 = 2;
 //or
-const [num1, num2] = [1, 2];
+const [num1, num2, ...otherNumbers] = [1, 2, 3, 4, 5];
 
-console.log(num1, num2);
+console.log(num1, num2, otherNumbers);
